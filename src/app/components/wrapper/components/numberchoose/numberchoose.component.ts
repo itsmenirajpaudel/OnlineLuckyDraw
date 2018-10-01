982e62ea1492faf549b0fbec588efe0c781ccc78
itsmenirajpaudel <niraj@Nirajs-machine.local>
2018-10-01 21:28:20 +0545

    changes

--- src/app/components/wrapper/components/numberchoose/numberchoose.component.ts
+++ src/app/components/wrapper/components/numberchoose/numberchoose.component.ts
@@ -1,17 +1,51 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { GlobalEvents } from "../../../../helpers/globalevents";
 import { Router } from "@angular/router";
 
 @Component({
   selector: "number-choose",
   templateUrl: "./numberchoose.component.html",
-  styles: []
+  styles: [
+    `
+      #one ul.numbers-list li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
-export class NumberChooseComponent {
+export class NumberChooseComponent implements OnInit {
+  numbers: any = [];
+  selectedNumberCommaSeparated = "";
+  selectedNumber: any = [];
   constructor(private globalEvents: GlobalEvents, private router: Router) {}
+
+  ngOnInit() {
+    for (let i = 0; i < 10; i++) {
+      this.numbers.push(i);
+    }
+  }
 
   goToDetails() {
     this.router.navigate(["/details"]);
     this.globalEvents.menuBarEvent.emit("details");
+  }
+
+  isDisabled(number) {
+    return (
+      this.selectedNumber.includes(number) || this.selectedNumber.length >= 4
+    );
+  }
+
+  selectNumber(number) {
+    if (!this.selectedNumber.includes(number)) {
+      this.selectedNumber.push(number);
+      this.selectedNumber.sort((a, b) => a - b);
+      this.selectedNumberCommaSeparated = this.selectedNumber.join(", ");
+    }
+  }
+
+  resetNumbers() {
+    this.selectedNumber = [];
+    this.selectedNumberCommaSeparated = "";
   }
 }
